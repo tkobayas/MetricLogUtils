0b3939cd73136fbab229a20aeb174f28c2c77425
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-06-16 14:33:06 +0900

    join use case

---
+++ use-cases/use-case-join/src/test/java/com/sample/JoinTest.java
@@ -0,0 +1,59 @@
+package com.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.core.reteoo.ReteDumper;
+import org.drools.core.util.PerfLogUtils;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class JoinTest {
+    
+    private final int NUM_CUSTOMER = 10;
+    private final int NUM_ORDER_PER_CUSTOMER = 100;
+    
+    @Test
+    public void testJoin() {
+
+        PerfLogUtils.setEnabled(true);
+        PerfLogUtils.setThreshold(-1);
+        
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kcontainer = ks.getKieClasspathContainer();
+        KieBase kbase = kcontainer.getKieBase();
+        KieSession ksession = kbase.newKieSession();
+        List<List<Order>> result = new ArrayList<>();
+        ksession.setGlobal("result", result);
+        
+        ReteDumper reteDumper = new ReteDumper();
+        reteDumper.dump(kbase);
+        System.out.println();
+        reteDumper.dumpAssociatedRules(kbase);
+        System.out.println();
+
+        for (int i = 0; i < NUM_CUSTOMER; i++) {
+            Customer cust = new Customer("Customer" + i);
+            ksession.insert(cust);
+            for (int j = 0; j < NUM_ORDER_PER_CUSTOMER; j++) {
+                long id = (i * NUM_ORDER_PER_CUSTOMER) + j;
+                int price = j * 10;
+                Order order = new Order(id, cust, "Item" + j, price);
+                ksession.insert(order);
+            }
+        }
+
+        System.out.println("-----------");
+        
+        long start = System.currentTimeMillis();
+        ksession.fireAllRules();
+        System.out.println("  -> elapsed time (ms) : " + (System.currentTimeMillis() - start));
+        
+        ksession.dispose();
+        
+        System.out.println("result.size() = " + result.size());
+    }
+}
