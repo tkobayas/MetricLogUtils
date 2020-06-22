c3712b55c00de7db0bb496dc67a0b72e5c2069a8
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-06-22 10:14:33 +0900

    bump to 7.40.0-SNAPSHOT

--- benchmark/kie-benchmarks/drools-benchmarks/run-quick.sh
+++ benchmark/kie-benchmarks/drools-benchmarks/run-quick.sh
@@ -1,13 +1,13 @@
 (cd dependency && sh install.sh)
 
 # without perf
-cp pom.xml.7.38.0-SNAPSHOT pom.xml
+cp pom.xml.7.40.0-SNAPSHOT pom.xml
 mvn clean install -DskipTests -Denforcer.fail=false
 
 java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -f 1 -i 1 -wi 0 -rf csv -rff results-without-perf.csv org.drools.benchmarks.session.FireOnlyWithJoinsBenchmark
 
 # with perf
-cp pom.xml.7.38.0-SNAPSHOT-perf pom.xml
+cp pom.xml.7.40.0-SNAPSHOT-perf pom.xml
 mvn clean install -DskipTests -Denforcer.fail=false
 
 java -jar target/drools-benchmarks.jar -jvmArgs "-Xms4g -Xmx4g" -foe true -f 1 -i 1 -wi 0 -rf csv -rff results-with-perf.csv org.drools.benchmarks.session.FireOnlyWithJoinsBenchmark
